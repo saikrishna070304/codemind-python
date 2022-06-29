293353be7d103f1f89887fc75a3e8177ab8160c3
saikrishna070304 <96769349+saikrishna070304@users.noreply.github.com>
2022-06-29 21:23:02 +0530

    n=int(input()) r=0 re=n n=abs(n) while n>0:     d=n%10     r=r*10+d     n=n//10 if re>0:     print(r) else:     print(-1*r)

--- Reverse_Integer.py
+++ Reverse_Integer.py
@@ -1,12 +1,12 @@
 n=int(input())
-rev=0
-nn=n
+r=0
+re=n
 n=abs(n)
 while n>0:
-    r=n%10
-    rev=rev*10+r
-    n//=10
-if nn>0:
-    print(rev)
+    d=n%10
+    r=r*10+d
+    n=n//10
+if re>0:
+    print(r)
 else:
-    print(-1*rev)
+    print(-1*r)

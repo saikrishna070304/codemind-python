0caacf6a22634c956daf0814694ae2931f8fc66b
saikrishna070304 <96769349+saikrishna070304@users.noreply.github.com>
2022-07-03 21:09:15 +0530

    n=int(input()) a=list(map(int,input().split())) b=[] for i in a:     c=0     if i==0:         c=1         b.append(c)         continue     if i<0:         i=i*(-1)     while i:         d=i%10         c+=1         i//=10     b.append(c) print(b.count(max(b)))

--- Max_digits_count.py
+++ Max_digits_count.py
@@ -1,14 +1,17 @@
-def dig(n):
-    c=0
-    while n:
-        i=n%10
-        n=n//10
-        c+=1
-    return c
 n=int(input())
 a=list(map(int,input().split()))
-l=[]
-for i in range(n):
-    l.append(dig(a[i]))
-x=max(l)
-print(l.count(x))
+b=[]
+for i in a:
+    c=0
+    if i==0:
+        c=1
+        b.append(c)
+        continue
+    if i<0:
+        i=i*(-1)
+    while i:
+        d=i%10
+        c+=1
+        i//=10
+    b.append(c)
+print(b.count(max(b)))    

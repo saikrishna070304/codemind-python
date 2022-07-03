e93b69a491657f15492bd3a59364a4f33a6234ea
saikrishna070304 <96769349+saikrishna070304@users.noreply.github.com>
2022-07-03 21:36:01 +0530

    def prime(n):     if n==1:         return 0     for i in range(2,(n//2)+1):         if n%i==0:             return 0     return 1 x = int(input()) l = list(map(int,input().split())) n = l.index(max(l)) m = l.index(min(l)) c=0 if m<n:     for i in range(m,n+1):         if prime(l[i]):             c+=1 else:     for i in range(n,m+1):         if prime(l[i]):             c+=1 print(c)

--- Primes_between_min_and_max.py
+++ Primes_between_min_and_max.py
@@ -1,21 +1,21 @@
-def isprime(n):
-    if n<2:
-        return False
-    else:
-        for i in range(2,int(n**0.5)+1):
-            if n%i==0:
-                return False
-        return True
-n=int(input())
-l=list(map(int,input().split()))
+def prime(n):
+    if n==1:
+        return 0
+    for i in range(2,(n//2)+1):
+        if n%i==0:
+            return 0
+    return 1
+x = int(input())
+l = list(map(int,input().split()))
+n = l.index(max(l))
+m = l.index(min(l))
 c=0
-d,f=l.index(min(l)),l.index(max(l))
-if d<f:
-    for i in range(d,f+1):
-        if isprime(l[i]):
+if m<n:
+    for i in range(m,n+1):
+        if prime(l[i]):
             c+=1
 else:
-    for i in range(f,d+1):
-        if isprime(l[i]):
+    for i in range(n,m+1):
+        if prime(l[i]):
             c+=1
 print(c)

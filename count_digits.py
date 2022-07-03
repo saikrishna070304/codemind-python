732f1323e9c0f66feeb5b1d149dbc13626de82a0
saikrishna070304 <96769349+saikrishna070304@users.noreply.github.com>
2022-07-03 21:12:54 +0530

    n=int(input()) l=list(map(int,input().split())) N=[] for i in l:     N.append(len(str(abs(i)))) print(*N) #print(l)

--- count_digits.py
+++ count_digits.py
@@ -1,15 +1,7 @@
-def dig(n):
-    if n==0:
-        return 1
-    if n<0:
-        n=n*-1
-    c=0
-    while n:
-        i=n%10
-        n=n//10
-        c+=1
-    return c
 n=int(input())
-a=list(map(int,input().split()))
-for i in range(n):
-    print(dig(a[i]),end=' ')
+l=list(map(int,input().split()))
+N=[]
+for i in l:
+    N.append(len(str(abs(i))))
+print(*N)
+#print(l)

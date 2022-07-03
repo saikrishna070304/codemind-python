eaf34882c86ace402797d3a2619e0a3fb53643f6
saikrishna070304 <96769349+saikrishna070304@users.noreply.github.com>
2022-07-03 21:18:32 +0530

    a=int(input()) b=list(map(int,input().split())) c=0 for i in range(0,len(b)-1):     if b[i]<b[i+1]:         c+=1 if c==len(b)-1:     print('yes') else:     print('no')

--- Ascending_array.py
+++ Ascending_array.py
@@ -1,12 +1,10 @@
-n = int(input())
-arr = list(map(int,input().split()))
-c = 0
-for i in range(0,n+1):# 4
-    if i==n-1:
-        break
-    if arr[i]<arr[i+1]:# 0 1
+a=int(input())
+b=list(map(int,input().split()))
+c=0
+for i in range(0,len(b)-1):
+    if b[i]<b[i+1]:
         c+=1
-if c==n-1:
-    print("yes")
+if c==len(b)-1:
+    print('yes')
 else:
-    print("no")
+    print('no')

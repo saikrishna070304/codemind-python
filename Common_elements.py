ffcdc78be19cc4b349b04e42433046c80f193210
saikrishna070304 <96769349+saikrishna070304@users.noreply.github.com>
2022-07-03 21:23:36 +0530

    a,b=map(int,input().split()) m=list(map(int,input().split())) n=list(map(int,input().split())) c=0 k=[] for i in sorted(set(m),key=m.index):     for j in set(n):         if i==j:             k.append(i) for f in k:     print(f,end=' ')

--- Common_elements.py
+++ Common_elements.py
@@ -1,8 +1,11 @@
 a,b=map(int,input().split())
 m=list(map(int,input().split()))
 n=list(map(int,input().split()))
-d=[]
-for i in m:
-    if i in n and i not in d:
-        d.append(i)
-print(*d)
+c=0
+k=[]
+for i in sorted(set(m),key=m.index):
+    for j in set(n):
+        if i==j:
+            k.append(i)
+for f in k:
+    print(f,end=' ')

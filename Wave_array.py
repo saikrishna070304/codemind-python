95cd14a006c9e1c36df9887c344bb7b6f943a72c
saikrishna070304 <96769349+saikrishna070304@users.noreply.github.com>
2022-07-03 21:19:06 +0530

    x = int(input()) l = list(map(int,input().split())) c=0 if l[0]<l[1]:     if (l[x-2]<l[x-1] and l[x-1]>l[0]):         c=1     else:         c=0     for i in range(1,x-1,2):         if (l[i-1]<l[i] and l[i]>l[i+1]):             c=1         else:             c=0             break elif l[0]>l[1]:     if (l[x-2]>l[x-1] and l[x-1]<l[0]):         c=1     else:         c=0     for i in range(1,x-1,2):         if (l[i-1]>l[i] and l[i]<l[i+1]):             c=1         else:             c=0             break if c==0:     print('no') else:     print('yes')

--- Wave_array.py
+++ Wave_array.py
@@ -1,23 +1,30 @@
+
 x = int(input())
 l = list(map(int,input().split()))
 c=0
 if l[0]<l[1]:
+    if (l[x-2]<l[x-1] and l[x-1]>l[0]):
+        c=1
+    else:
+        c=0
     for i in range(1,x-1,2):
-        if (l[i-1]<l[i] and l[i]>l[i-1]):
-            c+=1
+        if (l[i-1]<l[i] and l[i]>l[i+1]):
+            c=1
         else:
-            print('no')
             c=0
             break
-    if c!=0:
-        print("yes")
+elif l[0]>l[1]:
+    if (l[x-2]>l[x-1] and l[x-1]<l[0]):
+        c=1
+    else:
+        c=0
+    for i in range(1,x-1,2):
+        if (l[i-1]>l[i] and l[i]<l[i+1]):
+            c=1
+        else:
+            c=0
+            break
+if c==0:
+    print('no')
 else:
-    for i in range(1,x-1,2):
-        if (l[i-1]>l[i] and l[i]<l[i-1]):
-            c+=1
-        else:
-            print('no')
-            c=0
-            break
-    if c!=0:
-        print("yes")
+    print('yes')

beea934acaf10402a5f91483a1e1788645075dbb
saikrishna070304 <96769349+saikrishna070304@users.noreply.github.com>
2022-07-03 21:31:12 +0530

    def prime(n):     if n==1:         return 0     for i in range(2,int(n**0.5)+1):         if n%i==0:             return 0     return 1 n=int(input()) a=list(map(int,input().split())) c=0 d=0 for i in a:     if prime(i):         c+=i         d+=1 x=c/d print("%.2f"%x)

--- Average_of_primes.py
+++ Average_of_primes.py
@@ -1,15 +1,17 @@
-def isprime(n):
+def prime(n):
     if n==1:
-        return False
-    else:
-        for i in range(2,int(n**0.5)+1):
-            if n%i==0:
-                return False
-        return True
+        return 0
+    for i in range(2,int(n**0.5)+1):
+        if n%i==0:
+            return 0
+    return 1
 n=int(input())
-l=list(map(int,input().split()))
-N=[]
-for i in l:
-    if isprime(i):
-        N.append(i)
-print("{:.2f}".format(sum(N)/len(N)))
+a=list(map(int,input().split()))
+c=0
+d=0
+for i in a:
+    if prime(i):
+        c+=i
+        d+=1
+x=c/d
+print("%.2f"%x)

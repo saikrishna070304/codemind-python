3c5a2f46056bcd6c985e53b7d54ae0c1175d3f45
saikrishna070304 <96769349+saikrishna070304@users.noreply.github.com>
2022-07-03 21:18:03 +0530

    n=int(input()) a=list(map(int,input().split())) c=0 for i in a:     k=i     r=0     while(k):         d=k%10         r=r+d         k//=10     c=c+r print(c)

--- Play_with_digits.py
+++ Play_with_digits.py
@@ -1,13 +1,12 @@
-def sa(n):
-    s=0
-    while(n!=0):
-        r=n%10
-        s+=r
-        n=n//10
-    return s
 n=int(input())
-s=list(map(int,input().split()))
+a=list(map(int,input().split()))
 c=0
-for i in range(n):
-    c+=sa(s[i])
-print(c)
+for i in a:
+    k=i
+    r=0
+    while(k):
+        d=k%10
+        r=r+d
+        k//=10
+    c=c+r
+print(c)    
